41d1b2c9a74a1d55f471e20d393b884cbeaeccb0
Mathias Jørgensen <mathias@jorqensen.dev>
2026-04-14 14:31:02 +0200

    Add OpenCode to PATH

--- home/dot_config/private_fish/conf.d/paths.fish
+++ home/dot_config/private_fish/conf.d/paths.fish
@@ -2,4 +2,5 @@ set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH $HOME/.bun/bin $PATH
 set -gx PATH $HOME/.deno/bin $PATH
 set -gx PATH $HOME/.config/composer/vendor/bin $PATH
+set -gx PATH $HOME/.opencode/bin $PATH
 set -gx PATH /usr/local/go/bin $PATH
