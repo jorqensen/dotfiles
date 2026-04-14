a1a78b0d3bae1c6c0477e6373c25a15910a708cf
Mathias Jørgensen <mathias@jorqensen.dev>
2026-04-14 10:39:50 +0200

    Fixed installation script for Fish
    Uses /etc/os-release sourcing to grab the proper release key

--- home/.chezmoiscripts/run_before_01_install-packages.sh
+++ home/.chezmoiscripts/run_before_01_install-packages.sh
@@ -12,8 +12,9 @@ mkdir -p "$LOCAL_BIN"
 
 # Fish — Shell | https://fishshell.com/
 if ! command -v fish &> /dev/null; then
-    echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
-    curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
+    . /etc/os-release
+    echo "deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/${ID^}_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
+    curl -fsSL "https://download.opensuse.org/repositories/shells:fish:release:4/${ID^}_${VERSION_ID}/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
     sudo apt update
     sudo apt install -y fish
 fi
