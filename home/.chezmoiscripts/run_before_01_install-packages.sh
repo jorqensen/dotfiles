1d83803644d5f201c2956bc78b4c58b8e1195ae7
Mathias Jørgensen <mathias@jorqensen.dev>
2026-04-14 10:30:48 +0200

    Added -y flag to installation

--- home/.chezmoiscripts/run_before_01_install-packages.sh
+++ home/.chezmoiscripts/run_before_01_install-packages.sh
@@ -15,7 +15,7 @@ if ! command -v fish &> /dev/null; then
     echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
     curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
     sudo apt update
-    sudo apt install fish
+    sudo apt install -y fish
 fi
 
 # Micro — a modern terminal-based text editor | https://github.com/micro-editor/micro
